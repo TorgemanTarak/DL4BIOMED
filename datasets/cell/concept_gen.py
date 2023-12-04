932109d6ef753dd8156328427f573d0ef5a303b5
jozefcoldenhoff <jozef-c@hotmail.com>
2023-12-04 17:07:48 +0100

    PCA concepts

--- datasets/cell/concept_gen.py
+++ datasets/cell/concept_gen.py
@@ -8,13 +8,13 @@ import torch.nn.functional as F
 
 class LinkageConcepts:
 
-    def __init__(self, n_clusters, linkage_path, n_feats):
+    def __init__(self, n_concepts, data_path, n_feats, **kwargs):
         # Convert to n_clusters flat clusters
 
-        self.n_clusters = n_clusters
-        self.linkage_path = linkage_path
-        Z = np.load(linkage_path)
-        clusters = fcluster(Z, n_clusters, criterion='maxclust')
+        self.n_concepts = n_concepts
+        self.linkage_path = data_path
+        Z = torch.load(data_path)
+        clusters = fcluster(Z, n_concepts, criterion='maxclust')
 
         # subtract 1 from clusters to match python array indexing
         clusters = np.array(clusters) - 1
@@ -22,9 +22,21 @@ class LinkageConcepts:
         clusters = clusters.reshape(N, 1)
         clusters = np.array(clusters, dtype=np.float32)
         clusters = torch.from_numpy(clusters)
-        clusters = torch.nn.functional.one_hot(clusters.to(torch.int64), n_clusters).squeeze().transpose(0, 1)
+        clusters = torch.nn.functional.one_hot(clusters.to(torch.int64), n_concepts).squeeze().transpose(0, 1)
         self.mask = clusters
-        print("Generated concepts from linkage path", linkage_path)
+        print("Generated concepts from linkage path", data_path)
         # Sanity check
         assert clusters.shape[1] == n_feats, f"Clusters shape {clusters.shape} does not match n_feats {n_feats}"
 
+class PCAConcepts:
+
+  def __init__(self, n_concepts, data_path, n_feats, pca_top_k,**kwargs):
+    pca_components = torch.load(data_path)
+    
+    concepts = np.zeros((n_concepts, n_feats))
+
+    for i in range(n_concepts):
+      indices = np.argpartition(pca_components[i], -pca_top_k)[-pca_top_k:]
+      concepts[i, indices] = 1
+    
+    self.mask = concepts
