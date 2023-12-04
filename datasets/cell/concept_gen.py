Masking bugfix

datasets/cell/concept_gen.py
@@ -18,34 +18,33 @@ class LinkageConcepts:
 
         # subtract 1 from clusters to match python array indexing
         clusters = np.array(clusters) - 1
-        N = len(clusters)
-        clusters = clusters.reshape(N, 1)
-        clusters = np.array(clusters, dtype=np.float32)
-        clusters = torch.from_numpy(clusters)
-        clusters = torch.nn.functional.one_hot(clusters.to(torch.int64), n_concepts).squeeze().transpose(0, 1)
-        self.mask = clusters
+
+        mask = []
+        for i in range(n_concepts):
+            mask.append(np.where(clusters == i)[0])
+        self.mask = mask
+
         print("Generated concepts from linkage path", data_path)
-        # Sanity check
-        assert clusters.shape[1] == n_feats, f"Clusters shape {clusters.shape} does not match n_feats {n_feats}"
+        
 
 class PCAConcepts:
 
   def __init__(self, n_concepts, data_path, n_feats, pca_top_k,**kwargs):
     pca_components = torch.load(data_path)
     
-    concepts = np.zeros((n_concepts, n_feats))
+    mask = []
 
     for i in range(n_concepts):
       indices = np.argpartition(pca_components[i], -pca_top_k)[-pca_top_k:]
-      concepts[i, indices] = 1
+      mask.append(indices)
     
-    self.mask = concepts
+    self.mask = mask
 
 class NoConcepts:
     def __init__(self, n_concepts, data_path, n_feats, **kwargs):
-        self.mask = np.zeros((1, n_feats))
+        self.mask = np.arange(n_concepts).reshape(1, n_concepts)
 
 class RandomConcepts:
     
     def __init__(self, n_concepts, data_path, n_feats, **kwargs):
-        self.mask = np.random.randint(2, size=(n_concepts, n_feats))
+        self.mask = np.random.choice(n_feats, size=(n_concepts, 1), replace=False)